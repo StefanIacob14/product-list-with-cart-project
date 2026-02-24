1ddbfa9772cfb855ef73bf803f77c2062b3c3b8b
Stefan-Bogdan Iacob <biacob973@gmail.com>
2026-02-24 19:13:17 +0200

    Design implementation

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
 "use strict";
+
+// IMPORT ASSETS from "src" folder
+
+// Import icons from "src" folder
+const icons = import.meta.glob(`/src/assets/icons/*.svg`, {
+  eager: true,
+  import: `default`,
+});
+
+// Import images from "src" folder
+const images = import.meta.glob(`/src/assets/images/image-*.jpg`, {
+  eager: true,
+  import: `default`,
+});
+
+// VARIABLES & FUNCTIONS
+// Variables
+const emptyCartImg = document.getElementById(`empty-cart-img`);
+const confModalIcon = document.getElementById(`icon-order-confirmed`);
+
+// Functions
+
+// APPLICATION
+// Cart Section
+emptyCartImg.src = icons[`/src/assets/icons/illustration-empty-cart.svg`];
+
+// Confirmation Modal Window
+confModalIcon.src = icons[`/src/assets/icons/icon-order-confirmed.svg`];
